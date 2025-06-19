77cb2493405e81873fabb20d97261eb6a24651ef
rickyknight3000 <ricardovcapitao@hotmail.com>
2025-06-19 18:15:03 -0400

    added left and right rotation

--- main.py
+++ main.py
@@ -3,21 +3,23 @@ from constants import *
 from player import *
 
 def main():
+    
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
         player.draw(screen)
-
+        player.update(dt)
 
         pygame.display.flip()
-        clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
